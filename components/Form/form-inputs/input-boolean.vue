bfe349b231a8dd2bafccec42c6399317ed742451
Luiz Fernando de Lima Pereira <luiz.pereira@minutrade.com>
2018-09-27 18:40:35 -0300

    adicionando campo checkbox para value boolean

---
+++ components/Form/form-inputs/input-boolean.vue
@@ -0,0 +1,22 @@
+<template>
+  <input 
+    v-model="data[label]"
+    type="checkbox" >
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    label: { type: String, default: "" }
+  }
+};
+</script>
+
+<style>
+</style>
